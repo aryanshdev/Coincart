b7cd2e54d11115b8ca38c4aef3a49f9f936647e3
AryanshDev <aryanshappmaker@gmail.com>
2023-09-03 19:15:46 +0530

    Integrated EJS

--- main/app.js
+++ main/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(express.static(__dirname+ '/public'));
-app.listen(3000);
+app.listen(3001);
 
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname+"/index.html");
+    res.render(__dirname+"/index.ejs", {title:"CoinCart | Crypto Based Marketplace"});
 });
+
+app.get("/login", (req,res)=>{
+res.render(__dirname+"/login.ejs", {title:"Login | CoinCart"});
+});
+
+app.get("/register", (req,res) => {
+    res.render(__dirname+"/register.ejs" , {title:"Register | CoinCart"});
+})
