2728c78e5a772fe10b202c889366cce417723ee7
AryanshDev <aryanshappmaker@gmail.com>
2023-10-04 19:16:52 +0530

    Fixed Prices in All Pages

--- main/app.js
+++ main/app.js
@@ -20,23 +20,23 @@ app.get("/", (req, res) => {
   app.locals.item_in_cart = item_in_cart;
   var priceValues = [];
   fetch(
-    "ss",
+    "https://rest.coinapi.io/v1/exchangerate/USD?invert=true&filter_asset_id=bnb;btc;eth;ltc;sol;usdt",
     {
       headers: {
         "X-CoinAPI-Key": "D1F5B2A4-6DAF-4C46-BB24-08446E6C6BCE",
       },
     }
-  ).then(response => response.json())
-  .then((response) => {
-      console.log(response);
+  )
+    .then((response) => response.json())
+    .then((response) => {
       response.rates.forEach((element) => {
-        priceValues.push(Math.round(element.rate).toString()+ " USD");
+        priceValues.push(Math.round(element.rate).toString() + " USD");
       });
     })
     .catch(
-      (error) =>
-        (console.log(error),
-          priceValues = [
+      (error) => (
+        console.log(error),
+        (priceValues = [
           "Unable To Fetch Currently",
           "Unable To Fetch Currently",
           "Unable To Fetch Currently",
@@ -44,6 +44,7 @@ app.get("/", (req, res) => {
           "Unable To Fetch Currently",
           "Unable To Fetch Currently",
         ])
+      )
     )
     .then(() => {
       res.render(__dirname + "/index.ejs", {
@@ -60,109 +61,311 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   app.locals.item_in_cart = item_in_cart;
-  if (!username) {
-    res.render(__dirname + "/login.ejs", { title: "Login | CoinCart" });
-  } else {
-    res.redirect("/account");
-  }
-});
-
-app.get("/account", (req, res) => {
-  res.render(__dirname + "/account.ejs", { title: "Your Account | CoinCart" });
-});
-app.get("/register", (req, res) => {
-  app.locals.item_in_cart = item_in_cart;
-  res.render(__dirname + "/register.ejs", { title: "Register | CoinCart" });
-});
-
-app.get("/product-:id", (req, res) => {
-  app.locals.item_in_cart = item_in_cart;
-  database
-    .query("SELECT * FROM products WHERE id = " + req.params["id"] + ";")
-    .then((result) => {
-      result = result[0];
-      res.render(__dirname + "/ejs/product.ejs", {
-        title: result.product_name,
-        productName: result.product_name,
-        productImage: result.product_img,
-        category: result.product_category,
-        productPrice: result.product_price,
-        code: result.id,
-        description: result.product_description,
-        productDetails: result.details_tbl,
+  var priceValues = [];
+  fetch(
+    "https://rest.coinapi.io/v1/exchangerate/USD?invert=true&filter_asset_id=bnb;btc;eth;ltc;sol;usdt",
+    {
+      headers: {
+        "X-CoinAPI-Key": "D1F5B2A4-6DAF-4C46-BB24-08446E6C6BCE",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      response.rates.forEach((element) => {
+        priceValues.push(Math.round(element.rate).toString() + " USD");
       });
     })
-    .catch((error) => {
-      if (error.errno === -4039) {
-        res.render(__dirname + "/ejs/info-pg.ejs", {
-          title: "Connectivity Issue | CoinCart",
-          pageTitle: "There was a connection issue, please try again later",
-          message:
-            "<br><h6><a style='color:#ff7f00 'href='/'> Click Here To Return To HomePage </a> </h6>",
+    .catch(
+      (error) => (
+        console.log(error),
+        (priceValues = [
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+        ])
+      )
+    )
+    .then(() => {
+      if (!username) {
+        res.render(__dirname + "/login.ejs", {
+          title: "Login | CoinCart",
+          btcPrice: priceValues[1],
+          ethPrice: priceValues[2],
+          ltcPrice: priceValues[3],
+          bnbPrice: priceValues[0],
+          solPrice: priceValues[4],
+          usdtPrice: priceValues[5],
         });
-      } else if (error instanceof TypeError) {
-        res.render(__dirname + "/ejs/info-pg.ejs", {
-          title: "404 : Product Not Found",
-          pageTitle: "Product Not Found",
-          message:
-            "<br><h6><a style='color:#ff7f00 'href='/'> Click Here To Return To HomePage </a> </h6>",
-        });
+      } else {
+        res.redirect("/account");
       }
     });
 });
 
-app.get("/cart", (req, res) => {
-  if (username != null) {
-    //User Logged-In
-    var cartProducts = [];
-    database
-      .query("SELECT cart FROM users WHERE username = '" + username + "';")
-      .then((result) => {
-        if (result[0].cart !== null) {
-          //User Logged-In, Cart Not Empty
-          const cartProductsCode = result[0].cart.slice(0, -1).split(";");
-          const promises = [];
+app.get("/account", (req, res) => {
+  var priceValues = [];
+  fetch(
+    "https://rest.coinapi.io/v1/exchangerate/USD?invert=true&filter_asset_id=bnb;btc;eth;ltc;sol;usdt",
+    {
+      headers: {
+        "X-CoinAPI-Key": "D1F5B2A4-6DAF-4C46-BB24-08446E6C6BCE",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      response.rates.forEach((element) => {
+        priceValues.push(Math.round(element.rate).toString() + " USD");
+      });
+    })
+    .catch(
+      (error) => (
+        console.log(error),
+        (priceValues = [
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+        ])
+      )
+    )
+    .then(() => {
+      res.render(__dirname + "/account.ejs", {
+        title: "Your Account | CoinCart",
+        btcPrice: priceValues[1],
+        ethPrice: priceValues[2],
+        ltcPrice: priceValues[3],
+        bnbPrice: priceValues[0],
+        solPrice: priceValues[4],
+        usdtPrice: priceValues[5],
+      });
+    });
+});
 
-          cartProductsCode.forEach((code) => {
-            code = code.split(",");
-            const productQuery = database
-              .query("SELECT * FROM products WHERE id = " + code[0] + ";")
-              .then((productResult) => {
-                const product = productResult[0];
-                product.quantity = parseInt(code[1]);
-                return product;
+app.get("/register", (req, res) => {
+  var priceValues = [];
+  fetch(
+    "https://rest.coinapi.io/v1/exchangerate/USD?invert=true&filter_asset_id=bnb;btc;eth;ltc;sol;usdt",
+    {
+      headers: {
+        "X-CoinAPI-Key": "D1F5B2A4-6DAF-4C46-BB24-08446E6C6BCE",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      response.rates.forEach((element) => {
+        priceValues.push(Math.round(element.rate).toString() + " USD");
+      });
+    })
+    .catch(
+      (error) => (
+        console.log(error),
+        (priceValues = [
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+        ])
+      )
+    )
+    .then(() => {
+      app.locals.item_in_cart = item_in_cart;
+      res.render(__dirname + "/register.ejs", {
+        title: "Register | CoinCart",
+        btcPrice: priceValues[1],
+        ethPrice: priceValues[2],
+        ltcPrice: priceValues[3],
+        bnbPrice: priceValues[0],
+        solPrice: priceValues[4],
+        usdtPrice: priceValues[5],
+      });
+    });
+});
+
+app.get("/product-:id", (req, res) => {
+  var priceValues = [];
+  fetch(
+    "https://rest.coinapi.io/v1/exchangerate/USD?invert=true&filter_asset_id=bnb;btc;eth;ltc;sol;usdt",
+    {
+      headers: {
+        "X-CoinAPI-Key": "D1F5B2A4-6DAF-4C46-BB24-08446E6C6BCE",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      response.rates.forEach((element) => {
+        priceValues.push(Math.round(element.rate).toString() + " USD");
+      });
+    })
+    .catch(
+      (error) => (
+        console.log(error),
+        (priceValues = [
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+        ])
+      )
+    )
+    .then(() => {
+      app.locals.item_in_cart = item_in_cart;
+      database
+        .query("SELECT * FROM products WHERE id = " + req.params["id"] + ";")
+        .then((result) => {
+          result = result[0];
+          res.render(__dirname + "/ejs/product.ejs", {
+            title: result.product_name,
+            productName: result.product_name,
+            productImage: result.product_img,
+            category: result.product_category,
+            productPrice: result.product_price,
+            code: result.id,
+            description: result.product_description,
+            productDetails: result.details_tbl,
+
+            btcPrice: priceValues[1],
+            ethPrice: priceValues[2],
+            ltcPrice: priceValues[3],
+            bnbPrice: priceValues[0],
+            solPrice: priceValues[4],
+            usdtPrice: priceValues[5],
+          });
+        })
+        .catch((error) => {
+          if (error.errno === -4039) {
+            res.render(__dirname + "/ejs/info-pg.ejs", {
+              title: "Connectivity Issue | CoinCart",
+              pageTitle: "There was a connection issue, please try again later",
+              message:
+                "<br><h6><a style='color:#ff7f00 'href='/'> Click Here To Return To HomePage </a> </h6>",
+            });
+          } else if (error instanceof TypeError) {
+            res.render(__dirname + "/ejs/info-pg.ejs", {
+              title: "404 : Product Not Found",
+              pageTitle: "Product Not Found",
+              message:
+                "<br><h6><a style='color:#ff7f00 'href='/'> Click Here To Return To HomePage </a> </h6>",
+            });
+          }
+        });
+    });
+});
+
+app.get("/cart", (req, res) => {
+  var priceValues = [];
+  fetch(
+    "https://rest.coinapi.io/v1/exchangerate/USD?invert=true&filter_asset_id=bnb;btc;eth;ltc;sol;usdt",
+    {
+      headers: {
+        "X-CoinAPI-Key": "D1F5B2A4-6DAF-4C46-BB24-08446E6C6BCE",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      response.rates.forEach((element) => {
+        priceValues.push(Math.round(element.rate).toString() + " USD");
+      });
+    })
+    .catch(
+      (error) => (
+        console.log(error),
+        (priceValues = [
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+        ])
+      )
+    )
+    .then(() => {
+      if (username != null) {
+        //User Logged-In
+        var cartProducts = [];
+        database
+          .query("SELECT cart FROM users WHERE username = '" + username + "';")
+          .then((result) => {
+            if (result[0].cart !== null) {
+              //User Logged-In, Cart Not Empty
+              const cartProductsCode = result[0].cart.slice(0, -1).split(";");
+              const promises = [];
+
+              cartProductsCode.forEach((code) => {
+                code = code.split(",");
+                const productQuery = database
+                  .query("SELECT * FROM products WHERE id = " + code[0] + ";")
+                  .then((productResult) => {
+                    const product = productResult[0];
+                    product.quantity = parseInt(code[1]);
+                    return product;
+                  });
+
+                promises.push(productQuery);
               });
 
-            promises.push(productQuery);
-          });
+              return Promise.all(promises).then((cartProducts) => {
+                res.render(__dirname + "/cart.ejs", {
+                  title: "Cart",
+                  productsInCart: cartProducts,
+                  name: fullname,
 
-          return Promise.all(promises).then((cartProducts) => {
-            res.render(__dirname + "/cart.ejs", {
-              title: "Cart",
-              productsInCart: cartProducts,
-              name: fullname,
-            });
+                  btcPrice: priceValues[1],
+                  ethPrice: priceValues[2],
+                  ltcPrice: priceValues[3],
+                  bnbPrice: priceValues[0],
+                  solPrice: priceValues[4],
+                  usdtPrice: priceValues[5],
+                });
+              });
+            } else {
+              //User Logged-In, Cart Empty
+              res.render(__dirname + "/cart.ejs", {
+                title: "Cart",
+                productsInCart: [],
+                name: fullname,
+                btcPrice: priceValues[1],
+                ethPrice: priceValues[2],
+                ltcPrice: priceValues[3],
+                bnbPrice: priceValues[0],
+                solPrice: priceValues[4],
+                usdtPrice: priceValues[5],
+              });
+            }
+          })
+          .catch((error) => {
+            res.send(error);
           });
-        } else {
-          //User Logged-In, Cart Empty
-          res.render(__dirname + "/cart.ejs", {
-            title: "Cart",
-            productsInCart: [],
-            name: fullname,
-          });
-        }
-      })
-      .catch((error) => {
-        res.send(error);
-      });
-  } else {
-    //User Not Logged-In
-    res.render(__dirname + "/cart.ejs", {
-      title: "Cart",
-      productsInCart: [],
-      name: "",
+      } else {
+        //User Not Logged-In
+        res.render(__dirname + "/cart.ejs", {
+          title: "Cart",
+          productsInCart: [],
+          name: "",
+
+          btcPrice: priceValues[1],
+          ethPrice: priceValues[2],
+          ltcPrice: priceValues[3],
+          bnbPrice: priceValues[0],
+          solPrice: priceValues[4],
+          usdtPrice: priceValues[5],
+        });
+      }
     });
-  }
 });
 
 app.post("/login", (req, res) => {
@@ -187,7 +390,45 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/contact", (req, res) => {
-  res.render(__dirname + "/contact.ejs", { title: "Contact | CoinCart" });
+  var priceValues = [];
+  fetch(
+    "https://rest.coinapi.io/v1/exchangerate/USD?invert=true&filter_asset_id=bnb;btc;eth;ltc;sol;usdt",
+    {
+      headers: {
+        "X-CoinAPI-Key": "D1F5B2A4-6DAF-4C46-BB24-08446E6C6BCE",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      response.rates.forEach((element) => {
+        priceValues.push(Math.round(element.rate).toString() + " USD");
+      });
+    })
+    .catch(
+      (error) => (
+        console.log(error),
+        (priceValues = [
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+        ])
+      )
+    )
+    .then(() => {
+      res.render(__dirname + "/contact.ejs", {
+        title: "Contact | CoinCart",
+        btcPrice: priceValues[1],
+        ethPrice: priceValues[2],
+        ltcPrice: priceValues[3],
+        bnbPrice: priceValues[0],
+        solPrice: priceValues[4],
+        usdtPrice: priceValues[5],
+      });
+    });
 });
 
 app.post("/register", (req, res) => {
@@ -222,13 +463,8 @@ app.post("/register", (req, res) => {
 });
 
 app.get("/add-:id", (req, res) => {
-  console.log(
-    "UPDATE users SET cart = concat(cart , '" +
-      req.params["id"] +
-      "', ',1;') WHERE username = '" +
-      username +
-      "';"
-  );
+  var priceValues = [];
+
   database
     .query(
       "UPDATE users SET cart = concat(cart , '" +
@@ -257,12 +493,48 @@ app.post("/subscribe-form-footer", (req, res) => {
     )
     .catch((error) => res.send(error));
 });
-
 app.get("/shop", (req, res) => {
-  database.query("SELECT * FROM products;").then((result) => {
-    res.render(__dirname + "/ejs/shop.ejs", {
-      title: "Contact | CoinCart",
-      products: result,
+  var priceValues = [];
+  fetch(
+    "https://rest.coinapi.io/v1/exchangerate/USD?invert=true&filter_asset_id=bnb;btc;eth;ltc;sol;usdt",
+    {
+      headers: {
+        "X-CoinAPI-Key": "D1F5B2A4-6DAF-4C46-BB24-08446E6C6BCE",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      response.rates.forEach((element) => {
+        priceValues.push(Math.round(element.rate).toString() + " USD");
+      });
+    })
+    .catch(
+      (error) => (
+        console.log(error),
+        (priceValues = [
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+          "Unable To Fetch Currently",
+        ])
+      )
+    )
+    .then(() => {
+      database.query("SELECT * FROM products;").then((result) => {
+        res.render(__dirname + "/ejs/shop.ejs", {
+          title: "Contact | CoinCart",
+          products: result,
+
+          btcPrice: priceValues[1],
+          ethPrice: priceValues[2],
+          ltcPrice: priceValues[3],
+          bnbPrice: priceValues[0],
+          solPrice: priceValues[4],
+          usdtPrice: priceValues[5],
+        });
+      });
     });
-  });
 });
